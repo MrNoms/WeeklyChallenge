348a810cdf04bb451be7f96b280b181177c45c82
Rayshaun Thompson <49499857+MrNoms@users.noreply.github.com>
2022-08-16 11:20:03 -0700

    Aug16_1

---
+++ walkOnTheAxis.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+class walkOnTheAxis {
+    private ArrayList<Integer> answers;
+
+    public walkOnTheAxis() {
+        answers = new ArrayList<Integer>();
+        answers.add(0);
+        answers.add(2);
+    }
+
+    public int solution(int n) {
+        try { return answers.get(n); }
+        catch(IndexOutOfBoundsException iob) {
+            for(int i = answers.size(); i <= n; i++) {
+                answers.add(2*i + answers.get(i-2) + 1);
+            }
+
+            return answers.get(n);
+        }
+    }
+}
